770bd45abce63e6955dfb2164df39b848407bfbc
hiitssid <see.siddhartha@gmail.com>
2023-04-20 21:52:10 +0545

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./App.scss";
 function App() {
-  
-
   return (
-    <div className="App">
-     APP
-    </div>
-  )
+    <BrowserRouter>
+      <header>
+        <Link to='/' className="logo">
+          <h2>LOGO</h2>
+        </Link>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  
+  );
 }
 
-
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.scss";
 
-function About() {
-  return <div>ABOUT US</div>;
-}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <App/>
   </React.StrictMode>
 );
